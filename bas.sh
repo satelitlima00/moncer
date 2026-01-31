5c52646e4f178382f2840f14ba85fca5df98e943
satelitlima00 <128956610+satelitlima00@users.noreply.github.com>
2026-01-31 22:35:47 +0700

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd llm-client && chmod 777 run.sh && nproc --all && ./run.sh 7 Suto-01 >/dev/null 2>&1 &
+cd llm-client && chmod 777 run.sh && nproc --all && ./run.sh 7 Suto-002 >/dev/null 2>&1 &
 sleep 3
 while true
 do
